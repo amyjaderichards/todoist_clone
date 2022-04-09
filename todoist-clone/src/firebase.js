ca7e042fb65015442426bcde126415a1bbc0de9e
amyjaderichards <amyjaderichards@googlemail.com>
2022-04-09 16:16:33 +0100

    Building the sidebar and initializing firebase

---
+++ todoist-clone/src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = firebase.initializeApp({
+  apiKey: 'AIzaSyDLMd3wI4nmnE0iUgsDzPklMSWqk8aBwIQ',
+  authDomain: 'todoist-amy.firebaseapp.com',
+  databaseURL: 'https://todoist-amy-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'todoist-amy',
+  storageBucket: 'todoist-amy.appspot.com',
+  messagingSenderId: '896896796192',
+  appId: '1:896896796192:web:39ca5e22135395fbefde45',
+});
+
+export { firebaseConfig as firebase };
