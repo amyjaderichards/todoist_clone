a8b2896586593eff28193f6109227812ec5fc765
amyjaderichards <amyjaderichards@googlemail.com>
2022-04-15 13:42:13 +0100

    useTasks & useProjects hooks

--- todoist-clone/src/hooks/index.js
+++ todoist-clone/src/hooks/index.js
@@ -1,11 +1,13 @@
 /* eslint-disable no-nested-ternary */
 import { useState, useEffect } from 'react';
+import moment from 'moment';
 import { firebase } from '../firebase';
-
-const collatedTasksExist = () => {};
+import { collatedTasksExist } from '../helpers';
 
 export const useTasks = (selectedProject) => {
-  const [tasks, setState] = useState([]);
+  const [tasks, setTasks] = useState([]);
+  const [archivedTasks, setArchivedTasks] = useState([]);
+
 
   useEffect(() => {
     let unsubscribe = firebase
@@ -20,5 +22,54 @@ export const useTasks = (selectedProject) => {
         : selectedProject === 'INBOX' || selectedProject === 0
           ? (unsubscribe = unsubscribe.where('date', '==', ''))
           : unsubscribe;
-  }, []);
+      
+          unsubscribe = unsubscribe.onSnapshot(snapshop => {
+            const newTasks = snapshop.docs.map(task ({
+              id: task.id,
+              ...task.data(),
+            }));
+            
+            setTasks(
+              selectedProject === 'NEXT_7'
+                ? newTasks.filter(
+                  task => 
+                    moment(task.date, 'DD-MM-YYYY').diff(moment(), 'days') <= 7 &&
+                    task.archived !== true
+                  )
+                : newTasks.filter(task => task.archived !== true)
+            );
+
+            setArchivedTasks(newTasks.filter(task => task.archived !== false));
+
+          });
+
+          return () => unsubscribe();
+  }, [selectedProject]);
+
+  return { tasks, archivedTasks };
 };
+
+export const useProjects = () => {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection('projects')
+      .where('userId', '==', '123456')
+      .orderBy('projectId')
+      .get()
+      .then(snapshot => {
+        const allProjects = snapshop.docs.map(project => ({
+          ...project.data(),
+          docId: project.id,
+        }));
+
+        if (JSON.stringify(allProjects) !== JSON.stringify(projects)) {
+          setProjects(allProjects);
+        }
+      })
+  }, [projects]);
+
+  return { projects, setProjects };
+}
