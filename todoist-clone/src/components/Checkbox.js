616680702daab56320adae626441a6eec43a0d96
amyjaderichards <amyjaderichards@googlemail.com>
2022-04-15 14:30:53 +0100

    Checkbox component

---
+++ todoist-clone/src/components/Checkbox.js
@@ -0,0 +1,27 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable react/function-component-definition */
+import React from 'react';
+import { firebase } from '../firebase';
+
+export const Checkbox = ({ id }) => {
+  const archiveTask = () => {
+    firebase
+      .firestore()
+      .collection('tasks')
+      .doc(id)
+      .update({
+        archived: true,
+      });
+  };
+
+  return (
+    <div
+      className="checkbox-holder"
+      data-testid="checkbox-action"
+      onClick={() => archiveTask()}
+    >
+      <span className="checkbox" />
+    </div>
+  );
+};
